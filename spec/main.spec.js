0bb6a93c9d3e460cd10f0d76092d8df637455889
Muna <mmhussein125@gmail.com>
2019-02-07 00:17:40 +0000

    Unit test adding to chain

---
+++ spec/main.spec.js
@@ -0,0 +1,13 @@
+const { Block, Blockchain } = require("../main.js");
+
+describe("Blockchain", () => {
+
+  beforeEach(function() {
+    fakeCoin = new Blockchain();
+  });
+
+  it("Adds a new block to blockchain", () => {
+      fakeCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
+      expect((fakeCoin.chain.length)).toEqual(2);
+  });
+});
