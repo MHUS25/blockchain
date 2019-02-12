10a0b7cbe8297cf6e7c0fdee1e2779ab00f50cf5
Muna <mmhussein125@gmail.com>
2019-02-12 01:32:59 +0000

    Renames fakeCoin to litCoin

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -5,27 +5,27 @@ const {
 
 describe("Blockchain", () => {
   beforeEach(function() {
-    fakeCoin = new Blockchain();
+    litCoin = new Blockchain();
   });
 
   it("Adds a new block to blockchain", () => {
-    fakeCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
-    expect((fakeCoin.chain.length)).toEqual(2);
+    litCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
+    expect((litCoin.chain.length)).toEqual(2);
   });
 
   it("Can add more than one block to blockchain", () => {
-    fakeCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
-    fakeCoin.addBlock(new Block(2, "7/02/2019", { amount: 20 }));
-    expect((fakeCoin.chain.length)).toEqual(3);
+    litCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
+    litCoin.addBlock(new Block(2, "7/02/2019", { amount: 20 }));
+    expect((litCoin.chain.length)).toEqual(3);
   });
 
   it("Can check if a chain is valid", () => {
-    fakeCoin = new Blockchain();
-    fakeCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
-    fakeCoin.addBlock(new Block(2, "7/02/2019", { amount: 20 }));
-    console.log(fakeCoin);
-    expect((fakeCoin.isChainValid())).toEqual(true);
-    fakeCoin.chain[1].data = { amount: 100 };
-    expect((fakeCoin.isChainValid())).toEqual(false);
+    litCoin = new Blockchain();
+    litCoin.addBlock(new Block(1, "6/02/2019", { amount: 10 }));
+    litCoin.addBlock(new Block(2, "7/02/2019", { amount: 20 }));
+    console.log(litCoin);
+    expect((litCoin.isChainValid())).toEqual(true);
+    litCoin.chain[1].data = { amount: 100 };
+    expect((litCoin.isChainValid())).toEqual(false);
   });
 });
